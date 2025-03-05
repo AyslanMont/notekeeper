93cbd7c65f75913af2839212a52a99448a2b4360
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-03-05 14:43:29 -0300

    Validando formulário de autenticação e corrigindo bug das rotas não autorizadas

--- auth/auth.py
+++ auth/auth.py
@@ -2,17 +2,30 @@ from flask import Blueprint, request, render_template, redirect, url_for
 from notekeeper.models.User import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', url_prefix='/auth')
 
+class RegisterForm(FlaskForm):
+    name = StringField('Nome', validators=[DataRequired()])
+    email = EmailField('E-mail', validators=[DataRequired()])
+    passwd = PasswordField('Senha', validators=[DataRequired(), EqualTo('confirm_passwd', message='As senhas devem ser iguais.')])
+    confirm_passwd = PasswordField('Confirme a senha', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    email = EmailField('E-mail', validators=[DataRequired()])
+    passwd = PasswordField('Senha', validators=[DataRequired()])
+
 @auth_bp.route('/register', methods=["GET", "POST"])
 def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        passwd = form.passwd.data
 
-    if request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        passwd = request.form['passwd']
-        
         passwd = generate_password_hash(passwd)
 
         user = User.get_by_email(email)
@@ -22,22 +35,23 @@ def register():
             user.save()
             return redirect(url_for('auth.login'))
 
-    return render_template('register.html')
+    return render_template('register.html', form=form)
 
 @auth_bp.route('/login', methods=["GET", "POST"])
 def login():
-
-    if request.method == "POST":
-        email = request.form['email']
-        passwd = request.form['passwd']
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        passwd = form.passwd.data
 
         user = User.get_by_email(email)
 
         if user and check_password_hash(user.passwd, passwd):
-            login_user(user)
+            if user:
+                login_user(user)
             return redirect(url_for('note.list_notes'))
 
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 @auth_bp.route('/logout')
 def logout():
--- note/note.py
+++ note/note.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from notekeeper.models.Note import Note
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 note_bp = Blueprint('note', __name__, url_prefix='/notes', template_folder='templates')
 
-@login_required
+
 @note_bp.route('/create_note', methods=["GET", "POST"])
+@login_required
 def create_note():
     if request.method == "POST":
         title = request.form['title']
@@ -15,8 +19,8 @@ def create_note():
         new_note.save()
     return render_template("create_note.html")
 
-@login_required
 @note_bp.route('/list_notes', methods=["GET", "POST"])
+@login_required
 def list_notes():
     notes = Note.get_by_user(current_user.id)
     return render_template("list_notes.html", notes=notes)
